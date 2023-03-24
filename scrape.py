3b31ae9d76270d79c7f1bcd3392efd2f972db48b
paret0x <121698532+paret0x@users.noreply.github.com>
2023-03-24 17:13:34 -0400

    Update list and update whitelist
    Updated scrape script to find "good" bots (quantified by being modded by 10 channels in the last 24 hours)

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 known_bots = []
 whitelist = []
 new_bots = []
+new_good_bots = []
 
 with open("botlist.txt", 'r') as my_file:
     lines = my_file.readlines()
@@ -22,18 +23,40 @@ with open("new.txt", 'r') as my_file:
         if not ('\t' in bot_name):
             continue
             
-        bot_name = bot_name[:bot_name.index('\t')].strip().lower()
+        bot_fields = bot_name.strip().split("\t")
         
+        bot_name = bot_fields[0].strip().lower()
+        bot_mod_str = bot_fields[3].strip().replace(' ', '')
+        bot_mod_count = int(bot_mod_str)
+        
+        if bot_mod_count > 10:
+            if bot_name in known_bots:
+                known_bots.remove(bot_name)
+            
+            if not (bot_name in whitelist):
+                new_good_bots.append(bot_name)
+
+            continue
+            
         if not (bot_name in known_bots) and not (bot_name in whitelist) and not (bot_name in new_bots):
             new_bots.append(bot_name)
         
-print("Found %d new bots" % len(new_bots))
+print("Found %d new bots and %d new (probably) good bots" % (len(new_bots), len(new_good_bots)))
 
 for bot in new_bots:
     known_bots.append(bot)
 known_bots.sort()
 
+for bot in new_good_bots:
+    whitelist.append(bot)
+whitelist.sort()
+
 with open("botlist.txt", 'w') as out_file:
     for bot_name in known_bots:
+        out_file.write("%s\n" % bot_name)
+    out_file.close()
+
+with open("whitelist.txt", 'w') as out_file:
+    for bot_name in whitelist:
         out_file.write("%s\n" % bot_name)
     out_file.close()
