6e324506b217fc4cf508f35c883cfc46e0f369a4
paret0x <121698532+paret0x@users.noreply.github.com>
2023-05-02 18:08:20 -0400

    Scrape 05-02-2023

--- scrape.py
+++ scrape.py
@@ -22,28 +22,29 @@ with open("new.txt", 'r') as my_file:
         
         if not ('\t' in bot_name):
             continue
-            
-        bot_fields = bot_name.strip().split("\t")
-        
-        bot_name = bot_fields[0].strip().lower()
-        bot_mod_str = bot_fields[3].strip().replace(' ', '')
-        bot_mod_count = int(bot_mod_str)
-        
-        if len(bot_fields) < 5:
-            continue
-            
-        if bot_mod_count > 10:
-            if bot_name in known_bots:
-                known_bots.remove(bot_name)
-            
-            if not (bot_name in whitelist):
-                new_good_bots.append(bot_name)
 
+        bot_fields = bot_name.strip().split("\t")
+        if len(bot_fields) < 2:
             continue
             
+        bot_name = bot_fields[0].strip().lower()
+        
+        if len(bot_fields) == 5:
+            bot_mod_str = bot_fields[3].strip().replace(' ', '')
+            bot_mod_count = int(bot_mod_str)
+
+            if bot_mod_count > 10:
+                if bot_name in known_bots:
+                    known_bots.remove(bot_name)
+                
+                if not (bot_name in whitelist):
+                    new_good_bots.append(bot_name)
+
+                continue
+
         if not (bot_name in known_bots) and not (bot_name in whitelist) and not (bot_name in new_bots):
             new_bots.append(bot_name)
-        
+              
 print("Found %d new bots and %d new (probably) good bots" % (len(new_bots), len(new_good_bots)))
 
 for bot in new_bots:
